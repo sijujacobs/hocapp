6e05061ea3012c97fed5f6690b040070fde00d4f
SIJU JACOB <SIJUJACOBS@GMAILCOM>
2020-02-12 10:52:55 -0500

    Updates

---
+++ precache-manifest.e1b2b34d7f7e8e9f83f6d9906a164138.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3cf9e22971fa77538d4bb0eb8c019502",
+    "url": "/hocapp/index.html"
+  },
+  {
+    "revision": "e27f811fe36877bcc399",
+    "url": "/hocapp/static/css/main.e9c9b871.chunk.css"
+  },
+  {
+    "revision": "b579cd49811cb2d2d442",
+    "url": "/hocapp/static/js/2.84b93485.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/hocapp/static/js/2.84b93485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e27f811fe36877bcc399",
+    "url": "/hocapp/static/js/main.8a8dde15.chunk.js"
+  },
+  {
+    "revision": "fe14ceb0c2ef664cbaae",
+    "url": "/hocapp/static/js/runtime-main.8def636e.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/hocapp/precache-manifest.e1b2b34d7f7e8e9f83f6d9906a164138.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hocapp/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
